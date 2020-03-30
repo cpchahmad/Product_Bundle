8c0d06c0469bbed2f6aa70f007bc84dfab56a70e
Ahmad Naeem <cp.chahmad@gmail.com>
2020-03-30 16:19:03 +0500

    'aa'

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -29,4 +29,14 @@ class HelperController extends Controller
         $this->api->setAccessToken($this->getShop()->password);
         return $this->api;
     }
+    public function getShopifyDomain($domain){
+        $shop = User::where('name', $domain)->first();
+        $this->api = new BasicShopifyAPI();
+        $this->api->setApiKey(env('SHOPIFY_API_KEY'));
+        $this->api->setApiSecret(env('SHOPIFY_API_SECRET'));
+        $this->api->setVersion(env('SHOPIFY_API_VERSION'));
+        $this->api->setShop($shop->name);
+        $this->api->setAccessToken($shop->password);
+        return $this->api;
+    }
 }
